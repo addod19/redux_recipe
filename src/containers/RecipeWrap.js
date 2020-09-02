e6ac1b840c522d4f0ba74f52901bff21a804def0
Daniel Larbi Addo <addodaniellarbi@gmail.com>
2020-09-02 07:00:39 +0000

    fix console log errors

--- src/containers/RecipeWrap.js
+++ src/containers/RecipeWrap.js
@@ -6,7 +6,7 @@ import { MainWrap, ImageWrap, RecipeDetails } from '../styles/RecipeComponent';
 const ReWrap = ({ recipe }) => (
   <div className="showRecipe">
     <div className="text-center">
-      <MainWrap>
+      <MainWrap key={recipe.idMeal}>
         <ImageWrap>
           <img
             className="w-100 mb-2 recipeImg"
@@ -25,36 +25,9 @@ const ReWrap = ({ recipe }) => (
   </div>
 );
 
-// class ReWrap extends Component {
-//   render() {
-//     const { recipe } = this.props;
-//     return (
-//       <div className="showRecipe">
-//         <div className="text-center">
-//           <MainWrap>
-//             <ImageWrap>
-//               <img
-//                 className="w-100 mb-2 recipeImg"
-//                 src={recipe.strMealThumb}
-//                 alt="Recipe Cover"
-//                 title="click on image to view details"
-//               />
-//             </ImageWrap>
-//             <RecipeDetails>
-//               <h5 className="card-title name">
-//                 {recipe.strMeal}
-//               </h5>
-//             </RecipeDetails>
-//           </MainWrap>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
 ReWrap.propTypes = {
   recipe: PropTypes.shape({
-    idMeal: PropTypes.number,
+    idMeal: PropTypes.string,
     strMeal: PropTypes.string,
     strMealThumb: PropTypes.string,
   }).isRequired,
